c2e1c47dd0cb05c24f211f4af0fd20256518803b
andora814 <smith.doran@gmail.com>
2022-02-21 15:46:09 -0500

    commit before npm install

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -1,37 +1,25 @@
 // Navigation Bar SECTION
 const navBar = {
-  show: true,
+  show: true
 };
 
 // Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Hashir",
-  middleName: "",
-  lastName: "Shoaib",
-  message: " Passionate about changing the world with technology. ",
+  gradientColors: '#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1',
+  firstName: 'Doran',
+  middleName: '',
+  lastName: 'Smoth',
+  message: ' TBD. ',
   icons: [
     {
-      image: "fa-github",
-      url: "https://github.com/hashirshoaeb",
+      image: 'fa-github',
+      url: 'https://github.com/andora814'
     },
     {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/hashirshoaeb",
-    },
-    {
-      image: "fa-instagram",
-      url: "https://www.instagram.com/hashirshoaeb/",
-    },
-    {
-      image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/hashirshoaeb/",
-    },
-    {
-      image: "fa-twitter",
-      url: "https://www.twitter.com/hashirshoaeb/",
-    },
-  ],
+      image: 'fa-linkedin',
+      url: 'https://www.linkedin.com/in/dorancsmith/'
+    }
+  ]
 };
 
 // ABOUT SECTION
@@ -50,106 +38,107 @@ const mainBody = {
 
 const about = {
   show: true,
-  heading: "About Me",
-  imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  heading: 'About Me',
+  imageLink: require('../editable-stuff/doransmith.png'),
   imageSize: 375,
   message:
-    "My name is Hashir Shoaib. I’m a graduate of 2020 from National University of Sciences and Technology at Islamabad with a degree in Computer Engineering. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like working on open source projects.",
-  resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    'My name is Doran Smith. I am a Software Engineer with a degree in theoretical mathematics and a background in HR compensation analysis. I have experience working in a fast-paced and dynamic high-tech environment, and excel at collaborating and working on a team. I have a proven track record of communicating the most important details to non-technical audiences, allowing me to better message key takeaways, maintain engagement and facilitate decision making.',
+  resume:
+    'https://docs.google.com/document/d/1jfmtawXcWEnjPGUIZo2VdA53tVrTF1-OId8_JKLJevc/edit'
 };
 
 // PROJECTS SECTION
-// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
+// Setting up project length will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
 //      i.e: reposLength: 0,
 // If you want to display specfic projects, add the repository names,
 //      i.e ["repository-1", "repo-2"]
 const repos = {
   show: true,
-  heading: "Recent Projects",
-  gitHubUsername: "hashirshoaeb", //i.e."johnDoe12Gh"
+  heading: 'Recent Projects',
+  gitHubUsername: 'andora814', //i.e."johnDoe12Gh"
   reposLength: 4,
-  specificRepos: [],
+  specificRepos: []
 };
 
 // Leadership SECTION
-const leadership = {
-  show: false,
-  heading: "Leadership",
-  message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-  images: [
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-  ],
-  imageSize: {
-    width:"615",
-    height:"450"
-  }
-};
+// const leadership = {
+//   show: false,
+//   heading: "Leadership",
+//   message:
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+//   images: [
+//     {
+//       img: require("../editable-stuff/hashirshoaeb.png"),
+//       label: "First slide label",
+//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+//     },
+//     {
+//       img: require("../editable-stuff/hashirshoaeb.png"),
+//       label: "Second slide label",
+//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+//     },
+//   ],
+//   imageSize: {
+//     width:"615",
+//     height:"450"
+//   }
+// };
 
 // SKILLS SECTION
 const skills = {
   show: true,
-  heading: "Skills",
+  heading: 'Skills',
   hardSkills: [
-    { name: "Python", value: 90 },
-    { name: "SQL", value: 75 },
-    { name: "Data Structures", value: 85 },
-    { name: "C/C++", value: 65 },
-    { name: "JavaScript", value: 90 },
-    { name: "React", value: 65 },
-    { name: "HTML/CSS", value: 55 },
-    { name: "C#", value: 80 },
+    { name: 'Python', value: 90 },
+    { name: 'SQL', value: 75 },
+    { name: 'Data Structures', value: 85 },
+    { name: 'C/C++', value: 65 },
+    { name: 'JavaScript', value: 90 },
+    { name: 'React', value: 65 },
+    { name: 'HTML/CSS', value: 55 },
+    { name: 'C#', value: 80 }
   ],
   softSkills: [
-    { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
-    { name: "Positivity", value: 75 },
-    { name: "Adaptability", value: 85 },
-    { name: "Problem Solving", value: 75 },
-    { name: "Empathy", value: 90 },
-    { name: "Organization", value: 70 },
-    { name: "Creativity", value: 90 },
-  ],
+    { name: 'Goal-Oriented', value: 80 },
+    { name: 'Collaboration', value: 90 },
+    { name: 'Positivity', value: 75 },
+    { name: 'Adaptability', value: 85 },
+    { name: 'Problem Solving', value: 75 },
+    { name: 'Empathy', value: 90 },
+    { name: 'Organization', value: 70 },
+    { name: 'Creativity', value: 90 }
+  ]
 };
 
 // GET IN TOUCH SECTION
 const getInTouch = {
   show: true,
-  heading: "Get In Touch",
+  heading: 'Get In Touch',
   message:
     "I'm currently looking for full-time Software Engineering or Machine Learning opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "hashirshoaeb@gmail.com",
+  email: 'smith.doran@gmail.com'
 };
 
 const experiences = {
   show: false,
-  heading: "Experiences",
+  heading: 'Experiences',
   data: [
     {
-      role: 'Software Engineer',// Here Add Company Name
+      role: 'Software Engineer', // Here Add Company Name
       companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
+      date: 'June 2018 – Present'
     },
     {
       role: 'Front-End Developer',
       companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
-    },
+      date: 'May 2017 – May 2018'
+    }
   ]
-}
+};
 
 // Blog SECTION
 // const blog = {
 //   show: false,
 // };
 
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+export { navBar, mainBody, about, repos, skills, getInTouch, experiences };
